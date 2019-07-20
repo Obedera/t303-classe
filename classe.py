7375901fd7d89cbc403a742ffec90063aa1140c0
Obede <obede.silva7@gmail.com>
2019-07-20 12:03:01 -0300

    calcular idade pet

--- classe.py
+++ classe.py
@@ -1,3 +1,7 @@
+from datetime import date, datetime
+from math import floor
+
+
 class Pessoa:
     def __init__(self, nome, idade):
         self._nome = nome
@@ -21,15 +25,20 @@ class Pet(Pessoa):
     def __init__(self, dono,idade_dono, nome, data_nasc):
         super().__init__(dono,idade_dono)
         self.nome = nome
+        data_nasc = datetime.strptime(data_nasc, '%d/%m/%Y').date()
         self.data_nasc = data_nasc
     
     def get_dono(self):
         return super().get_nome()
     
+    def get_idade_pet(self):
+        data_atual = datetime.now().date()
+        idade =  int((data_atual-self.data_nasc).days)
+        return floor((idade/365))
+    
 
 
 obede = Pessoa('Obede', '15')
-costela = Pet('Obede', '15', 'costela', '2018')
+costela = Pet('Obede', '15', 'costela', '25/09/2017')
 
-
-print(costela.get_dono())
+print(costela.get_idade_pet())
