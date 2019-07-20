90960d7a3e01c5ba96a9d4eed4cf63e6a17d8c9f
Obede <obede.silva7@gmail.com>
2019-07-20 12:39:31 -0300

    Instanciar classes

--- classe.py
+++ classe.py
@@ -1,11 +1,10 @@
 from datetime import date, datetime
 from math import floor
 
-
 class Pessoa:
     def __init__(self, nome, idade):
         self._nome = nome
-        self.idade = idade
+        self._idade = idade
     
     def set_nome(self, valor):
         self._nome = valor
@@ -18,7 +17,9 @@ class Pessoa:
     
     def get_idade(self):
         return self._idade
-
+    
+    def falar(self):
+        return print('Talkey')
 
 
 class Pet(Pessoa):
@@ -28,17 +29,41 @@ class Pet(Pessoa):
         data_nasc = datetime.strptime(data_nasc, '%d/%m/%Y').date()
         self.data_nasc = data_nasc
     
+    def get_nome(self):
+        return self.nome
+    
+    def set_nome(self, valor):
+        self.nome = valor
+
     def get_dono(self):
         return super().get_nome()
+
+    def set_dono(self, valor):
+        super().set_nome(valor)
+
+    def set_data_nasc(self, valor):
+        data_nasc = datetime.strptime(valor, '%d/%m/%Y').date()
+        self.data_nasc = data_nasc
     
-    def get_idade_pet(self):
+    def get_idade(self):
         data_atual = datetime.now().date()
         idade =  int((data_atual-self.data_nasc).days)
         return floor((idade/365))
-    
+
+    def get_dados(self):
+        print(f'O nome do dono é {self.get_dono()} e sua idade é {super().get_idade()} anos')
+        print(f'O nome do pet é {self.get_nome()} e sua idade é {self.get_idade()}')
 
 
 obede = Pessoa('Obede', '15')
-costela = Pet('Obede', '15', 'costela', '25/09/2017')
+costela = Pet('Obede', '15', 'Costela', '25/09/2017')
 
-print(costela.get_idade_pet())
+costela.set_nome('Costelinha')
+costela.set_dono('Obedera')
+
+costela.get_dados()
+obede.falar()
+
+costela.set_data_nasc('25/09/2016')
+
+costela.get_dados()
