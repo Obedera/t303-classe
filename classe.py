0a0e593920d10896ef3a23ac0a17f0ebac120efd
Obede <obede.silva7@gmail.com>
2019-07-19 23:18:40 -0300

    classe pet

--- classe.py
+++ classe.py
@@ -17,8 +17,19 @@ class Pessoa:
 
 
 
-
+class Pet(Pessoa):
+    def __init__(self, dono,idade_dono, nome, data_nasc):
+        super().__init__(dono,idade_dono)
+        self.nome = nome
+        self.data_nasc = data_nasc
+    
+    def get_dono(self):
+        return super().get_nome()
     
 
 
 obede = Pessoa('Obede', '15')
+costela = Pet('Obede', '15', 'costela', '2018')
+
+
+print(costela.get_dono())
